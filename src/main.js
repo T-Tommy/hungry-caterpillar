f96a96eb7a656013d14e2c4b716ec0531cc38d05
David Nishikawa <david@squishymedia.com>
2019-02-09 15:53:06 -0800

    modify syntax: iffe wrapper, forEach, arrow func, closure, ternary, object prop bracket notation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-main();
-
-function main() {
+// giant iffe
+(() => {
     const fruitList = [
         'coconut',
         'jackfruit',
@@ -8,79 +7,77 @@ function main() {
         'guava',
         'mangosteen'
     ];
-    makeButtons(fruitList, document.getElementById('fruit-field'));
-    makeButtons(fruitList, document.getElementById('dance-field'));
-}
-
-function makeButtons(list, element) {
-    // Create buttoms
-    for(let i = 0; i < list.length; i++) {
-        const listItem = list[i];
-        const button = makeButtonEl(element, listItem);
-
-        // Place fruit image on button
-        if(element.id === 'fruit-field') {
-            insertIcon(button, listItem);
-        }
-
-        // Button functionality
-        button.addEventListener('click', function() {
-            buttonAction(element, listItem);
+    
+    main(fruitList, document.getElementById('fruit-field'));
+    main(fruitList, document.getElementById('dance-field'));
+    
+    function main(list, element) {
+        // Create buttoms
+        // the forEach array method takes each of it's elements and passes it through the callback function
+        // could also look like this v
+        // array.forEach(function(element) {
+        //     /* do stuff with each element here */
+        // })
+        list.forEach(listItem => {
+            const button = makeButton(element, listItem);
+    
+            // Place fruit image on button
+            if(element.id === 'fruit-field') insertIcon(button, listItem);
+    
+            // Button functionality
+            button.addEventListener('click', buttonAction(element, listItem));
         });
     }
-}
-
-function makeButtonEl(element, listItem) {
-    const button = document.createElement('button');
-    button.classList.add('button', listItem);
-    button.textContent = listItem;
-    element.appendChild(button);
-    return button;
-}
-
-function insertIcon(button, listItem) {
-    const buttonIcon = document.createElement('img');
-    buttonIcon.src = './assets/' + listItem + '.jpg';
-    buttonIcon.classList.add('fruit-image');
-    button.textContent = '';
-    button.appendChild(buttonIcon);
-}
-
-function buttonAction(element, listItem) {            
-    switch(element.id) {
-        case 'fruit-field':
-            addBody(listItem);
-            break;
-        case 'dance-field':
-            dance(listItem);
-            break;
+    
+    function makeButton(element, listItem) {
+        const button = document.createElement('button');
+        button.classList.add('button', listItem);
+        button.textContent = listItem;
+        element.appendChild(button);
+        return button;
     }
-}
-
-// Add body segment on caterpillar
-function addBody(fruit) {
-    const segment = document.createElement('span');
-    segment.classList.add('segment', fruit);
-    document.getElementById('caterpillar-body').appendChild(segment);
-}
-
-function dance(fruit) {
-    reset();
-    move(fruit);
-}
-
-// Reset Position by removing 'dance' class from all segments
-function reset() {
-    const allSegments = document.querySelectorAll('.dance');
-    for(let i = 0; i < allSegments.length; i++) {
-        allSegments[i].classList.remove('dance');
+    
+    function insertIcon(button, listItem) {
+        const buttonIcon = document.createElement('img');
+        buttonIcon.src = './assets/' + listItem + '.jpg';
+        buttonIcon.classList.add('fruit-image');
+        button.textContent = '';
+        button.appendChild(buttonIcon);
     }
-}
-
-// Add 'dance' class to selected segments to move them up
-function move(fruit) {
-    const selectedSegments = document.querySelectorAll('.segment.' + fruit);
-    for(let i = 0; i < selectedSegments.length; i++) {
-        selectedSegments[i].classList.add('dance');
+    
+    function buttonAction(element, listItem) {
+        // returns a function that is called when the event fires
+        // if the element.id is 'fruit-field' it executes addBody(), otherwise, dance()
+        return () => (element.id === 'fruit-field') ? addBody(listItem) : dance(listItem);
     }
-}
+    
+    // Add body segment on caterpillar
+    function addBody(fruit) {
+        const segment = document.createElement('span');
+        segment.classList.add('segment', fruit);
+        document.getElementById('caterpillar-body').appendChild(segment);
+    }
+    
+    function dance(fruit) {
+        reset();
+        move(fruit);
+    }
+    
+    const setDanceClass = (selection, remove) => {
+        const action = remove ? 'remove' : 'add';
+        selection.forEach(segment => segment.classList[action]('dance'));
+    };
+    
+    // Reset Position by removing 'dance' class from all segments
+    function reset() {
+        const allSegments = document.querySelectorAll('.dance');
+        setDanceClass(allSegments, true);
+    }
+    
+    // Add 'dance' class to selected segments to move them up
+    function move(fruit) {
+        const selector = '.segment.' + fruit;
+        const selectedSegments = document.querySelectorAll(selector);
+        setDanceClass(selectedSegments);
+    }
+})();
