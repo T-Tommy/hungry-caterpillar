a0f6d45777ce681f2ea9042800c6e1a0752849b5
Tommy Tran <tranttommy95@gmail.com>
2019-02-09 14:43:58 -0800

    further organized into more functions but broke

--- src/main.js
+++ src/main.js
@@ -1,53 +1,56 @@
-const fruitList = [
-    'coconut',
-    'jackfruit',
-    'rambutan',
-    'guava',
-    'mangosteen'
-];
+const fruitList = ['coconut', 'jackfruit', 'rambutan', 'guava', 'mangosteen'];
 
 main(fruitList, document.getElementById('fruit-field'));
 main(fruitList, document.getElementById('dance-field'));
 
-function main(list, element) {
+function main(list, fieldElement) {
     // Create buttoms
     for(let i = 0; i < list.length; i++) {
         const listItem = list[i];
-        const button = document.createElement('button');
-        button.classList.add('button', listItem);
-        button.textContent = listItem;
-        element.appendChild(button);
-
+        makeButton(listItem, fieldElement);
+        
         // Place fruit image on button
-        if(element.id === 'fruit-field') {
-            insertIcon(button, listItem);
+        const buttonElement = document.getElementsByTagName('button')[i];
+        if(fieldElement.id === 'fruit-field') {
+            insertIcon(buttonElement, listItem);
         }
-
-        // Button functionality
-        button.addEventListener('click', function() {
-            buttonAction(element, listItem);
+        buttonElement.addEventListener('click', function() {
+            switch(fieldElement.id) {
+                case 'fruit-field':
+                    addBody(listItem);
+                    break;
+                case 'dance-field':
+                    dance(listItem);
+                    break;
+            }
+            // buttonAction(fieldElement, listItem);
         });
     }
 }
 
-function insertIcon(button, listItem) {
+function makeButton(listItem, fieldElement) {
+    const buttonElement = document.createElement('button');
+    buttonElement.classList.add('button', listItem);
+    buttonElement.textContent = listItem;
+    fieldElement.appendChild(buttonElement);
+}
+function insertIcon(buttonElement, listItem) {
     const buttonIcon = document.createElement('img');
     buttonIcon.src = './assets/' + listItem + '.jpg';
     buttonIcon.classList.add('fruit-image');
-    button.textContent = '';
-    button.appendChild(buttonIcon);
+    buttonElement.textContent = '';
+    buttonElement.appendChild(buttonIcon);
 }
 
-function buttonAction(element, listItem) {            
-    switch(element.id) {
-        case 'fruit-field':
-            addBody(listItem);
-            break;
-        case 'dance-field':
-            dance(listItem);
-            break;
-    }
-}
+// function buttonAction(fieldElement, listItem) {            
+//     switch(fieldElement.id) {
+//         case 'fruit-field':
+//             addBody(listItem);
+//             break;
+//         case 'dance-field':
+//             dance(listItem);
+//             break;
+// }
 
 // Add body segment on caterpillar
 function addBody(fruit) {
